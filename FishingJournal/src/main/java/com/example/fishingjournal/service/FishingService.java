717bf9b9d62d95f2e0c208c7d2cb5b907264be75
Adam <adam.szanto1@gmail.com>
2023-07-10 16:31:25 +0200

    Finished form and basic auth

--- FishingJournal/src/main/java/com/example/fishingjournal/service/FishingService.java
+++ FishingJournal/src/main/java/com/example/fishingjournal/service/FishingService.java
@@ -1,4 +1,11 @@
 package com.example.fishingjournal.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class FishingService {
+    private final static String TOKEN_VALUE = "1337tokenvalue";
+    public boolean isValidAuth(String tokenValue) {
+        return tokenValue.equals(TOKEN_VALUE);
+    }
 }
