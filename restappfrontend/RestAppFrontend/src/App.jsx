35fe5bbf30c15b7bc32c4a461cb9683cadf88cc9
Adam <adam.szanto1@gmail.com>
2023-07-13 17:46:30 +0200

    frontend

--- restappfrontend/RestAppFrontend/src/App.jsx
+++ restappfrontend/RestAppFrontend/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import React, { useState } from 'react';
+import DoctorTable from './DoctorTable.jsx';
+import DoctorDetails from './DoctorDetails.jsx';
 
-function App() {
-  return (
-      <div className = "app">
-        This is Adam's code.
-      </div>
-  )
-}
 
-export default App
+const App = () => {
+    const [selectedDoctor, setSelectedDoctor] = useState(null);
+
+    const handleSelectDoctor = doctor => {
+        setSelectedDoctor(doctor);
+    };
+
+    return (
+        <div>
+            <h1>List of Doctors</h1>
+            <DoctorTable onSelectDoctor={handleSelectDoctor} />
+            {selectedDoctor && <DoctorDetails doctor={selectedDoctor} />}
+        </div>
+    );
+};
+
+export default App;
