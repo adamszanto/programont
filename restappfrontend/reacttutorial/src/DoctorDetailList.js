0c3eca0a993bef163dc6e8fed04d7b85b901e15a
Adam <adam.szanto1@gmail.com>
2023-07-16 17:24:42 +0200

    Finishing touch

--- restappfrontend/reacttutorial/src/DoctorDetailList.js
+++ restappfrontend/reacttutorial/src/DoctorDetailList.js
@@ -1,49 +1,60 @@
-import {Contactheader} from "./contactheader";
+import { Contactheader } from "./contactheader";
 import Contact from "./contact";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import DoctorData from "./DoctorData";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import PatientContact from "./patient";
-import {Patientheader} from "./patientheader";
+import { Patientheader } from "./patientheader";
+import {Footer} from "./footer";
+import BackTo from "./BackTo";
 
 export function DoctorDetailList() {
-    const {id} = useParams();
-    const [doctor, setDoctor] = useState({patients: []});
+    const params = useParams();
+    const location = useLocation();
+    const [doctor, setDoctor] = useState({ patients: [] });
 
-    useEffect(()=> {
+    useEffect(() => {
+        console.log('Doctor id: ', location.state.doctorId);
+        console.log('Params: ', params)
         const fetchData = async () => {
             try {
-                const response = await fetch(`http://localhost:8080/api/doctors/${id}`);
-                if(!response.ok) {
+                  const response = await fetch(`http://localhost:8080/api/doctors/${location.state.doctorId}`);
+                if (!response.ok) {
                     throw new Error("Couldn't process request");
                 }
                 const data = await response.json();
-                setDoctor(data.Doctor);
+                
+                setDoctor(data);
             } catch (error) {
                 console.error("Error happened", error);
             }
         };
         fetchData();
-    }, [id]);
+    }, []);
 
     return (
-        <div className="doctorList">
+        <div className="main-div">
             <Contactheader />
             <Contact
-            key={doctor.id}
-            doctorId={doctor.id}
-            name={doctor.name}
-            specialization={doctor.specialization}
-            patientNum={doctor.patients.length}
+                key={doctor.id}
+                doctorId={doctor.id}
+                name={doctor.name}
+                specialization={doctor.specialization}
+                patientNum={doctor.patients.length}
             />
-                <Patientheader />
-                {doctor.patients.map((patient) => (
-                    <PatientContact
-                        key={patient.id}
-                        name={patient.name}
-                        birthDate={patient.birthDate}
-                    />
-                ))}
+            <div>
+            <Patientheader />
+            {doctor.patients.map((patient) => (
+                <PatientContact
+                    key={patient.id}
+                    id={patient.id}
+                    name={patient.name}
+                    birthDate={patient.birthDate}
+                />
+            ))}
             </div>
+            <BackTo />
+            <Footer />
+        </div>
     );
 }
--- restappfrontend/reacttutorial/src/contact.js
+++ restappfrontend/reacttutorial/src/contact.js
@@ -1,21 +1,32 @@
 import React from "react"
-import {useState} from "react"
+import { useState } from "react"
 import PatientContact from "./patient";
-import {Patientheader} from "./patientheader";
-import { Link } from 'react-router-dom';
+import { Patientheader } from "./patientheader";
 
-export default function Contact({doctorId, name, specialization, patients, patientNum}) {
+
+import { Link } from "react-router-dom";
+
+
+export default function Contact({ doctorId, name, specialization, patients, patientNum }) {
 
     return (
-        <Link to={`/doctors/${doctorId}`}>
-        <div>
-            <div className="tableRow" >
-                <div className="tableCell">{doctorId}</div>
-                <div className="tableCell">{name}</div>
-                <div className="tableCell">{specialization}</div>
-                <div className="tableCell">{patientNum}</div>
+        <Link
+            to={`/doctor/${doctorId}`}
+            state={{
+                doctorId,
+                name,
+                specialization,
+                patients,
+                patientNum
+            }}>
+            <div>
+                <div className="tableRow" >
+                    <div className="tableCell">{doctorId}</div>
+                    <div className="tableCell">{name}</div>
+                    <div className="tableCell">{specialization}</div>
+                    <div className="tableCell">{patientNum}</div>
+                </div>
             </div>
-        </div>
         </Link>
     )
 }
