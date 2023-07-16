fe59b8d3ee2ed84f0ee9ea0158b85c0433c05451
Adam <adam.szanto1@gmail.com>
2023-07-16 14:08:48 +0200

    router works, but no data

--- restappfrontend/reacttutorial/src/DoctorDetailList.js
+++ restappfrontend/reacttutorial/src/DoctorDetailList.js
@@ -2,53 +2,48 @@ import {Contactheader} from "./contactheader";
 import Contact from "./contact";
 import React, {useEffect, useState} from "react";
 import DoctorData from "./DoctorData";
+import { useParams } from "react-router-dom";
 import PatientContact from "./patient";
 import {Patientheader} from "./patientheader";
 
 export function DoctorDetailList() {
-    const [doctors, setDoctors] = useState([]);
+    const {id} = useParams();
+    const [doctor, setDoctor] = useState({patients: []});
 
     useEffect(()=> {
         const fetchData = async () => {
             try {
-                const response = await fetch("http://localhost:8080/api/doctors");
+                const response = await fetch(`http://localhost:8080/api/doctors/${id}`);
                 if(!response.ok) {
                     throw new Error("Couldn't process request");
                 }
                 const data = await response.json();
-                setDoctors(data);
+                setDoctor(data.Doctor);
             } catch (error) {
                 console.error("Error happened", error);
             }
         };
         fetchData();
-    }, []);
+    }, [id]);
 
-    const doctorElements = doctors.map(doctor => {
-        return
-        <div>
-        <Contact
+    return (
+        <div className="doctorList">
+            <Contactheader />
+            <Contact
             key={doctor.id}
             doctorId={doctor.id}
             name={doctor.name}
             specialization={doctor.specialization}
             patientNum={doctor.patients.length}
-        />
-            <Patientheader />
-            {doctor.patients.map((patient) => (
-                <PatientContact
-                    key={patient.id}
-                    name={patient.name}
-                    birthDate={patient.birthDate}
-                />
-            ))}
-
-        </div>
-    })
-    return(
-        <div className="doctorList">
-            <Contactheader />
-            {doctorElements}
-        </div>
-    )
+            />
+                <Patientheader />
+                {doctor.patients.map((patient) => (
+                    <PatientContact
+                        key={patient.id}
+                        name={patient.name}
+                        birthDate={patient.birthDate}
+                    />
+                ))}
+            </div>
+    );
 }
