5929a9ec146a618352edb993da5578c1b201311f
Adam <adam.szanto1@gmail.com>
2023-07-16 10:57:57 +0200

    Implementing dynamic api fetch

---
+++ restappfrontend/reacttutorial/src/patientheader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css';
+export function Patientheader() {
+    return(
+        <div>
+            <div className="tableRowHeader">
+                <div className="tableCell">Name</div>
+                <div className="tableCell">Date of birth</div>
+            </div>
+        </div>
+    )
+}
