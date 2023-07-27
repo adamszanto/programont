3dd21e0368eac1fe9932fb8a200d62ab1f3f2983
Adam <adam.szanto1@gmail.com>
2023-07-27 10:50:08 +0200

    Rest API laid down

--- Retro Gaming Rental/src/main/java/com/example/retro/RentingController.java
+++ Retro Gaming Rental/src/main/java/com/example/retro/RentingController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -19,11 +21,12 @@ public class RentingController {
     }
 
     private void initializeGames() {
-        Game game1 = new Game("Donkey Kong Country", 1994, Platform.SNES, true);
+        Game game1 = new Game("Donkey Kong Country", 1994, Platform.SNES, "https://images.igdb.com/igdb/image/upload/t_cover_big/co6pym.png", true);
+        Game game2 = new Game("Gran Turismo 2", 1999, Platform.PLAYSTATION, "https://images.igdb.com/igdb/image/upload/t_cover_big/co20af.png", true);
 
         games.add(game1);
+        games.add(game2);
     }
-
 
     @GetMapping
     public ResponseEntity<List<Game>> gameList() {
@@ -34,5 +37,12 @@ public class RentingController {
             }
         }
         return new ResponseEntity<>(availableGames, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addGame(@RequestBody Game game) {
+        games.add(game);
+
+        return new ResponseEntity<>("Game added successfully: " +  game.toString(), HttpStatus.OK);
     }
 }
