90b41af3d3a385b2799a5d8e8022c82fcdc5e878
Adam <adam.szanto1@gmail.com>
2023-07-28 17:26:29 +0200

    Separated renting and game list

--- Retro Gaming Rental/src/main/java/com/example/retro/service/RentingService.java
+++ Retro Gaming Rental/src/main/java/com/example/retro/service/RentingService.java
@@ -1,63 +1,86 @@
 package com.example.retro.service;
 
-import com.example.retro.controller.RentingController;
+import com.example.retro.repository.CurrentlyRentingEntity;
+import com.example.retro.repository.CurrentlyRentingRepository;
 import com.example.retro.repository.GameEntity;
 import com.example.retro.repository.RentingRepository;
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class RentingService {
     private final RentingRepository rentingRepository;
+    private final CurrentlyRentingRepository currentlyRentingRepository;
     private final ModelMapper modelMapper;
 
-    public RentingService(RentingRepository rentingRepository, ModelMapper modelMapper) {
+    public RentingService(RentingRepository rentingRepository, CurrentlyRentingRepository currentlyRentingRepository, ModelMapper modelMapper) {
         this.rentingRepository = rentingRepository;
+        this.currentlyRentingRepository = currentlyRentingRepository;
         this.modelMapper = modelMapper;
     }
 
     public List<Game> getAvailableGames() {
-        List<GameEntity> games = rentingRepository.findAllByRentable(true);
-        List<Game> gameList = games.stream()
+        List<GameEntity> allGames = rentingRepository.findAll();
+        List<Long> rentedGameIds = currentlyRentingRepository.findAll()
+                .stream()
+                .map(CurrentlyRentingEntity::getGameId)
+                .collect(Collectors.toList());
+
+        List<Long> availableGameIds = allGames.stream()
+                .map(GameEntity::getId)
+                .filter(gameId -> !rentedGameIds.contains(gameId))
+                .collect(Collectors.toList());
+
+        return allGames.stream()
+                .filter(game -> availableGameIds.contains(game.getId()))
                 .map(game -> modelMapper.map(game, Game.class))
                 .collect(Collectors.toList());
-        return gameList;
+
+//        return availableGameIds.stream()
+//                .map(gameId -> rentingRepository.findById(gameId))
+//                .filter(Optional::isPresent)
+//                .map(Optional::get)
+//                .map(game -> modelMapper.map(game, Game.class))
+//                .collect(Collectors.toList());
     }
 
     public List<Game> getAllGames() {
-        List<GameEntity> gameEntities = rentingRepository.findAll();
-        List<Game> gameList = gameEntities.stream()
+        List<GameEntity> allGames = rentingRepository.findAll();
+        return allGames.stream()
                 .map(game -> modelMapper.map(game, Game.class))
                 .collect(Collectors.toList());
-        return gameList;
     }
+
+    public List<CurrentlyRenting> getAllRenting() {
+        List<CurrentlyRentingEntity> allRenting = currentlyRentingRepository.findAll();
+        return allRenting.stream()
+                .map(renting -> modelMapper.map(renting, CurrentlyRenting.class))
+                .collect(Collectors.toList());
+    }
+
 
     public Game findGameById(Long id) {
         GameEntity game = rentingRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Game with id " + id + " not found"));
         return modelMapper.map(game, Game.class);
     }
 
-    public boolean rentGame(Long id) {
+    public CurrentlyRentingEntity rentGame(Long id, String email) {
         Optional<GameEntity> optionalGame = rentingRepository.findById(id);
-        if(optionalGame.isPresent()) {
+        Optional<CurrentlyRentingEntity> optionalCurrentRenting = currentlyRentingRepository.findById(id);
+
+        if(optionalGame.isPresent() && !optionalCurrentRenting.isPresent()) {
             GameEntity game = optionalGame.get();
-            if(game.isRentable()) {
-                game.setRentable(false);
-                rentingRepository.save(game);
-                return true;
-            } else {
-                return false;
-            }
+
+            CurrentlyRentingEntity currentlyRentingEntity = new CurrentlyRentingEntity();
+            currentlyRentingEntity.setGameId(game.getId());
+            currentlyRentingEntity.setEmail(email);
+            return currentlyRentingRepository.save(currentlyRentingEntity);
+        } else {
+            return null;
         }
-        return false;
     }
 
     public Game addGame(Game game) {
