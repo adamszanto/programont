d72b75706430ea69664fc4db360b4b9fb8989605
Szántó Ádám <szanto.adam1@ext.telekom.hu>
2023-12-20 14:37:21 +0100

    Kafka integration start

--- CoinManager/src/main/java/com/example/coinmanager/config/KafkaTopicConfig.java
+++ CoinManager/src/main/java/com/example/coinmanager/config/KafkaTopicConfig.java
@@ -1,11 +1,15 @@
-//package com.example.coinmanager.config;
-//
-//@Configuration
-//public class KafkaTopicConfig {
-//
-//    @Bean
-//    public NewTopic coinDataTopic(@Value("${coin.manager.kafka.topic}") String kafkaTopic) {
-//        return TopicBuilder.name(kafkaTopic)
-//                .build();
-//    }
-//}
+package com.example.coinmanager.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic coinDataTopic(@Value("${coin.manager.kafka.topic}") String kafkaTopic) {
+        return TopicBuilder.name(kafkaTopic)
+                .build();
+    }
+}
