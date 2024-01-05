6c1d63d058c8f43c89a9fed56894ff85d7af9deb
adamszanto <51482667+adamszanto@users.noreply.github.com>
2024-01-05 16:06:28 +0100

    Delete nasanevnap directory

--- nasanevnap/CoinManager/src/main/java/com/example/coinmanager/repository/PriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.coinmanager.repository;
-
-import com.example.coinmanager.service.model.CurrencyPriceData;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PriceRepository extends MongoRepository<CurrencyPriceData, String> {
-}
